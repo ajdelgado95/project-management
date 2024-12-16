195d25d545ce7306704b1e34ddcdb10d48818a7a
Alfonso Delgado <alfonsofuentes@reps2.com>
2024-12-16 00:19:07 -0600

    Add tasks model and migration Add custom command to generate fake data Add tasks to urls and api serializers

--- backend-site/app/api/serializers.py
+++ backend-site/app/api/serializers.py
@@ -1,15 +1,23 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from app.project.models import Project
+from app.project.models import Project, Task
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
 
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
 class ProjectSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
     updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+    tasks = TaskSerializer(many=True, read_only=True)
     class Meta:
         model = Project
         fields = '__all__'
@@ -19,3 +27,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'projects', 'project_teams']
+
--- backend-site/app/project/models.py
+++ backend-site/app/project/models.py
@@ -9,3 +9,14 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, related_name='tasks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
